e38d6be08ae98cdd4be77e11bfa003897d1b95bd
Andrew Friedman <andrewfriedman01@gmail.com>
2019-08-25 22:18:17 -0400

    Completed L10

---
+++ Lab10/Rectangles/Rectangles/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+    Class:	    CSE 1321L
+    Section:    16  
+    Term:		Fall 2019 
+    Instructor:	Rehnuma Afrin
+    Name:		Andrew Friedman   
+    Lab#:		10
+*/
+
+using System;
+
+namespace Rectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle object1 = new Rectangle();
+            Rectangle object2 = new Rectangle(5, 6);
+            Console.WriteLine("First object: ");
+            object1.getHeight();
+            object1.getWidth();
+            object1.getArea();
+            object1.getPerimeter();
+            Console.WriteLine("Second object: ");
+            object2.getHeight();
+            object2.getWidth();
+            object2.getArea();
+            object2.getPerimeter();
+        }
+    }
+
+    class Rectangle
+    {
+        double height;
+        double width;
+        public Rectangle()
+        {
+            height = 1.0;
+            width = 1.0;
+        }
+        public Rectangle(double newHeight, double newWidth)
+        {
+            height = newHeight;
+            width = newWidth;
+        }
+        public void getArea()
+        {
+            Console.WriteLine("Area: " + height * width);
+        }
+        public void getPerimeter()
+        {
+            Console.WriteLine("Perimeter: " + (height + width) * 2);
+        }
+        public void getHeight()
+        {
+            Console.WriteLine("Height: " + height);
+        }
+        public void getWidth()
+        {
+            Console.WriteLine("Width: " + width);
+        }
+    }
+}
---
+++ Lab10/Stocks/Stocks/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+    Class:	    CSE 1321L
+    Section:    16  
+    Term:		Fall 2019 
+    Instructor:	Rehnuma Afrin
+    Name:		Andrew Friedman   
+    Lab#:		10
+*/
+
+using System;
+
+namespace Stocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock Google = new Stock("Google", "GOG");
+            Google.setClosingPrice(134.67);
+            Google.setCurrentPrice(131.98);
+
+            Stock Microsoft = new Stock("Microsoft", "MSF");
+            Microsoft.setClosingPrice(156.52);
+            Microsoft.setCurrentPrice(161.22);
+
+            Console.WriteLine(Google.getName() + " stock:");
+            Console.WriteLine("Symbol: " + Google.getSymbol());
+            Console.WriteLine("Closing price: " + Google.previousClosingPrice);
+            Console.WriteLine("Current price: " + Google.currentPrice);
+            Console.WriteLine("Change percent: " + Google.getChangePercent() + "%");
+            Google.toString();
+            Console.WriteLine();
+
+            Console.WriteLine(Microsoft.getName() + " stock:");
+            Console.WriteLine("Symbol: " + Microsoft.getSymbol());
+            Console.WriteLine("Closing price: " + Microsoft.previousClosingPrice);
+            Console.WriteLine("Current price: " + Microsoft.currentPrice);
+            Console.WriteLine("Change percent: " + Microsoft.getChangePercent() + "%");
+            Microsoft.toString();
+        }
+    }
+
+    class Stock
+    {
+        string Symbol;
+        string Name;
+        public double previousClosingPrice;
+        public double currentPrice;
+        public Stock(string newName, string newSymbol)
+        {
+            Name = newName;
+            Symbol = newSymbol;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public string getSymbol()
+        {
+            return Symbol;
+        }
+        public void setClosingPrice(double currentClosingPrice)
+        {
+            previousClosingPrice = currentClosingPrice;
+        }
+        public void setCurrentPrice(double newPrice)
+        {
+            currentPrice = newPrice;
+        }
+        public double getChangePercent()
+        {
+            return (currentPrice - previousClosingPrice) / currentPrice * 100;
+        }
+        public void toString()
+        {
+            Console.WriteLine(Name + " stock closing price is $" + currentPrice);
+        }
+    }
+}
