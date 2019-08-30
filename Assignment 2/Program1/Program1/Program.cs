06c48c2ca8847eb6d4d8898cec4290f2e36684a8
Andrew Friedman <andrewfriedman01@gmail.com>
2019-08-30 13:11:28 -0400

    Completed A2: source P1 & P2

---
+++ Assignment 2/Program1/Program1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an amount in USD: $");
+            float USD = float.Parse(Console.ReadLine());
+            Console.WriteLine("You have " + USD * 9240 + " Guinean Francs");
+        }
+    }
+}
---
+++ Assignment 2/Program2/Program2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user input
+            Console.Write("Entered number: ");
+            int n = int.Parse(Console.ReadLine());
+            // calculation
+            int result = 0;
+            for (int i = 1; i <= 1000; i *= 10) // iterate by times 10 until 4 digits reached
+            {
+                result += (n % (i * 10) - n % i) / i; // add value of first x digits minus x-1 digits
+            }
+            // output
+            Console.WriteLine("Sum of digits: " + result);
+        }
+    }
+}
