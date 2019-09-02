2a9cbac26f3261cfd9958fbe9ae2f5c473f77a01
Andrew Friedman <andrewfriedman01@gmail.com>
2019-09-01 23:58:06 -0400

    Completed A2 Pseudo & Source

---
+++ Assignment 2/Program3/Program3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float x1, y1, x2, y2;
+			Console.Write("Entered X1: ");
+			x1 = float.Parse(Console.ReadLine());
+			Console.Write("Entered Y1: ");
+			y1 = float.Parse(Console.ReadLine());
+			Console.Write("Entered X2: ");
+			x2 = float.Parse(Console.ReadLine());
+			Console.Write("Entered Y2: ");
+			y2 = float.Parse(Console.ReadLine());
+			Console.WriteLine("Distance: " + Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+		}
+	}
+}
---
+++ Assignment 2/Program4/Program4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the flight distance: ");
+            int distance = int.Parse(Console.ReadLine());
+            Console.Write("Enter the current cost of jet fuel: ");
+            float costFuel = float.Parse(Console.ReadLine());
+            Console.WriteLine("The flight will cost $" + distance * costFuel * 5 + " in jet fuel");
+            Console.Write("Enter the number of passengers: ");
+            int passengers = int.Parse(Console.ReadLine());
+            Console.Write("Enter the average cost of a ticket: ");
+            int costTicket = int.Parse(Console.ReadLine());
+            Console.WriteLine("You will make a profit of $" + (passengers * costTicket - distance * costFuel * 5));
+            Console.WriteLine("You averaged " + passengers / 5 + " miles per person per gallon!");
+        }
+    }
+}
