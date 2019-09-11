af0a5257592d028ad7317e0185b8830d074cd7bb
Andrew Friedman <andrewfriedman01@gmail.com>
2019-09-10 22:04:49 -0400

    Completed A3:Pseudocode
    - added pseudocode
    - changed "sallary" variable to "income" on Source:P4
    - optimized Source:P4

--- Assignment 3/Source Code/Program4/Program4/Program.cs
+++ Assignment 3/Source Code/Program4/Program4/Program.cs
@@ -4,32 +4,31 @@ namespace Program4
 {
     class Program
     {
-        static int sallary;
+        static int income;
         static void Main(string[] args)
         {
             Console.Write("Enter your salary to the nearest dollar: ");
-            sallary = int.Parse(Console.ReadLine());
-            float tax = 0;
-            tax += taxAdder(9525, 0, .1f);
+            income = int.Parse(Console.ReadLine());
+            float tax = taxAdder(9525, 0, .1f);
             tax += taxAdder(38700, 9525, .12f);
             tax += taxAdder(82500, 38700, .22f);
             tax += taxAdder(157500, 82500, .24f);
             tax += taxAdder(200000, 157500, .32f);
             tax += taxAdder(500000, 200000, .35f);
             tax += taxAdder(0, 500000, .37f);
-            Console.Write("Total tax owed is: $" + tax);
+            Console.Write("Total tax owed is: $" + (int)tax);
         }
 
         static float taxAdder (int amount, int previous, float rate)
         {
-            if (sallary > amount - previous && amount != 0)
+            if (income > amount - previous && amount != 0)
             {
-                sallary -= amount - previous;
+                income -= amount - previous;
                 return (amount - previous) * rate;
             }else
             {
-                int temp = (int)(sallary * rate);
-                sallary = 0;
+                int temp = (int)(income * rate);
+                income = 0;
                 return temp;
             }
         }
